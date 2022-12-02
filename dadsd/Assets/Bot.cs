cafdd4187feb67274d5f887d66e8964dde3806d3
Jinam503 <jinam1234567@gmail.com>
2022-12-02 23:29:21 +0900

    221202

--- dadsd/Assets/Bot.cs
+++ dadsd/Assets/Bot.cs
@@ -5,16 +5,19 @@ using UnityEngine.UI;
 
 public class Bot : MonoBehaviour
 {
+    public float CurHp = 100f;
+    private float MaxHp = 100f;
     public Canvas uiCanvas;
     private Image hpbarImage;
 
     public GameObject hpBarPrefab;
-    public Vector3 hpBarOffset = new Vector3(0, 2.2f, 0);
+    public Vector3 hpBarOffset = new Vector3(0, 1.0f, 0);
     private void Start()
     {
+        CurHp = 100;
         SetHpBar();
     }
-    void SetHpBar()
+    private void SetHpBar()
     {
         GameObject hpbar = Instantiate(hpBarPrefab, uiCanvas.transform);
         hpbarImage = hpbar.GetComponentsInChildren<Image>()[1];
@@ -23,7 +26,19 @@ public class Bot : MonoBehaviour
         _hpbar.target = gameObject.transform;
         _hpbar.offset = hpBarOffset;
     }
-
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Sword")
+        {
+            Debug.Log(CurHp);
+            CurHp -= collision.gameObject.GetComponent<Sword>().damage;
+            hpbarImage.fillAmount = CurHp / MaxHp;
+            if(CurHp <= 0)
+            {
+                CurHp = 100F;
+            }
+        }
+    }
 }
 
 
--- dadsd/Assets/Scripts/PlayerController.cs
+++ dadsd/Assets/Scripts/PlayerController.cs
@@ -126,18 +126,10 @@ public class PlayerController : MonoBehaviour
     }
     private bool IsCheckGrounded()
     {
-        
-        // CharacterController.IsGrounded가 true라면 Raycast를 사용하지 않고 판정 종료
         if (characterController.isGrounded) return true;
-        // 발사하는 광선의 초기 위치와 방향
-        // 약간 신체에 박혀 있는 위치로부터 발사하지 않으면 제대로 판정할 수 없을 때가 있다.
         var ray = new Ray(this.transform.position + Vector3.up * 0.1f, Vector3.down);
-        // 탐색 거리
         var maxDistance = 0.2f;
-        // 광선 디버그 용도
         Debug.DrawRay(transform.position + Vector3.up * 0.1f, Vector3.down * maxDistance, Color.red);
-        // Raycast의 hit 여부로 판정
-        // 지상에만 충돌로 레이어를 지정
         return Physics.Raycast(ray, maxDistance, layerMask);
     }
     public void CanAttack()
