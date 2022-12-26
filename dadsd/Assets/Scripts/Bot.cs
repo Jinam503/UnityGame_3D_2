e6c2d41d0b1443492324055c39bbd4d45a29c3bb
Jinam503 <jinam1234567@gmail.com>
2022-12-27 00:00:11 +0900

    221227

--- dadsd/Assets/Scripts/Bot.cs
+++ dadsd/Assets/Scripts/Bot.cs
@@ -12,34 +12,10 @@ public class Bot : MonoBehaviour
     BoxCollider boxCollider;
 
 
-    //체력바 띄우기
-    public Canvas uiCanvas;
-    private Image hpbarImage;
-    public GameObject hpBarPrefab;
-    public Vector3 hpBarOffset = new Vector3(0, 1.0f, 0);
-    public Vector3 DamageNumOffset;
-    public GameObject damageImage;
     private void Awake()
     {
         rigid = GetComponent<Rigidbody>();
         boxCollider = GetComponent<BoxCollider>();
-    }
-    private void Start()
-    {
-        SetHpBar();
-    }
-    private void Update()
-    {
-        DamageNumOffset = new Vector3(Random.Range(1.0f, 0f), Random.Range(1.0f, 0f), 0);
-    }
-    private void SetHpBar()
-    {
-        GameObject hpbar = Instantiate(hpBarPrefab, uiCanvas.transform);
-        hpbarImage = hpbar.GetComponentsInChildren<Image>()[1];
-
-        var _hpbar = hpbar.GetComponent<Hpbar>();
-        _hpbar.target = gameObject.transform;
-        _hpbar.offset = hpBarOffset;
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -47,21 +23,7 @@ public class Bot : MonoBehaviour
         {
             Weapon weapon = other.GetComponent<Weapon>();
             CurHp -= weapon.damage;
-            StartCoroutine(ShowDamage(weapon.damage));
         }
-    }
-    
-    IEnumerator ShowDamage(int damage)
-    {
-        GameObject damageNum = Instantiate(damageImage, uiCanvas.transform);
-        TextMesh tM = damageNum.GetComponent<TextMesh>();
-        string numString = "" + damage;
-        tM.text = numString;
-        var _dN = damageNum.GetComponent<DamageNum>();
-        _dN.target = gameObject.transform;
-        _dN.offset = DamageNumOffset;
-        yield return new WaitForSeconds(2f);
-        Destroy(damageNum);
     }
 }
 
